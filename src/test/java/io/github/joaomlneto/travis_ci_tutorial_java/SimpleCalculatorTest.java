3df6dab4ffc582cd272ec41a4f54b60a0879adfc
onnebang <mwon0408@gmail.com>
2021-05-23 22:46:37 +0900

    실패한 테스트케이스

--- src/test/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,21 +9,22 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.add(1, 1), 3);
 	}
 	@Test
 	public void testSub() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.sub(1, 1), 0);
+		assertEquals(calc.sub(1, 1), 2);
 	}
 	@Test
 	public void testMul() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.mul(3, 5), 15);
+		assertEquals(calc.mul(3, 5), 10);
 	}
 	@Test
 	public void testDiv() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.div(8, 2), 4);
+		assertEquals(calc.div(8, 2), 3);
+		
 	}
 }
